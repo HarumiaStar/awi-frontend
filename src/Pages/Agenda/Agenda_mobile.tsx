11ab8af35f566eb4d539679faca528a3e47992e5
Ciberdead <nico_goyon@orange.fr>
2024-01-07 20:39:54 +0100

    correctif affichage mobile

--- src/Pages/Agenda/Agenda_mobile.tsx
+++ src/Pages/Agenda/Agenda_mobile.tsx
@@ -2,26 +2,38 @@ import { Liste_agenda } from "./Classes/Liste_Agenda"
 import { Liste_deroulante, Liste_deroulante_element, Liste_deroulante_element_data } from "../../Utils/Liste_deroulante"
 import { Creneaux } from "../../Utils/Creneaux";
 import SmallDate from "../../Utils/SmallDate";
+import { ThreeDMap, TwoDMap } from "../../Utils/ThreeDMap";
+import { v4 } from "uuid";
 
 
 export default function Agenda_mobile({ agenda }: { agenda: Liste_agenda }) {
-    const liste_deroulante: Liste_deroulante_element[] = agenda.mapDate(new SmallDate(10,10,2010), (value: [string, Map<Creneaux, number>]) => {
-        const creneaux = value[1];
+    const sections : {nom: string, liste: Liste_deroulante_element[]}[] = []
 
-        const data: Liste_deroulante_element_data[] = [];
+    agenda.liste.get2DByB().forEach((activite: TwoDMap<string, Creneaux, number>, date: SmallDate) => {
+        const liste_deroulante: Liste_deroulante_element[] = []
+        const nom_section = date.frFormat
 
-        creneaux.forEach((value: number, key: Creneaux) => {
-            data.push({ nom: key, valeur: value })
+        activite.getMapWithFirstDimension().forEach((creneau: Map<Creneaux, number>, nom_activite: string) => {
+            const liste_deroulante_element_data: Liste_deroulante_element_data[] = []
+            creneau.forEach((valeur: number, creneau: Creneaux) => {
+                liste_deroulante_element_data.push(new Liste_deroulante_element_data(creneau, valeur))
+            })
+            liste_deroulante.push(new Liste_deroulante_element(nom_activite, liste_deroulante_element_data))
         })
 
-        return new Liste_deroulante_element(value[0], data);
+        sections.push({nom: nom_section, liste: liste_deroulante})
+    })
 
+    const section_affichage = sections.map((section, index) => {
+        const liste_deroulante = <Liste_deroulante liste_objet={section.liste} />
+        return <section key={v4()} className="section_liste_deroulante">
+            <h3>{section.nom}</h3>
+            {liste_deroulante}
+        </section>
     })
 
     return <>
         <h2>Agenda sur mobile</h2>
-
-        {/* Liste sous forme d'une première case avec le nom de l'activité, puis une liste dépliante avec chaque créneau */}
-        <Liste_deroulante liste_objet={liste_deroulante} />
+        {section_affichage}
     </>
 }
